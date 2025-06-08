3bcea4a9c3e3ca2b98e43856da1670922b579ca0
Jb Carreon <jbcarreon212@gmail.com>
2025-06-08 21:24:23 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	if (navigator.storage && navigator.storage.persist) {
+		navigator.storage.persist().then((persistent) => {
+			if (persistent) {
+				console.log('Storage will not be cleared except by explicit user action');
+			} else {
+				console.log('Storage may be cleared by the UA under storage pressure.');
+			}
+		});
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
